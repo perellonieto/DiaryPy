9297d1f8bb1f5daf654a2cb868e3bc84cc58121e
miquel_perello_nieto <perello.nieto@gmail.com>
2020-02-07 14:24:56 +0000

    Refactored setup - Removed requirements that are standard from Python - Read version from __init__ - Added README as long description

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from distutils.core import setup
+from distutils.util import convert_path
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+main_ns = {}
+ver_path = convert_path('diarypy/__init__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
 setup(
   name = 'diarypy',
   packages = ['diarypy'],
   install_requires=[
-    'os',
-    'sys',
-    'errno',
-    'csv',
     'datetime',
     'Image',
   ],
-  version = '0.3.1',
+  version=main_ns['__version__'],
   description = 'Diary to create notebooks and store intermediate results and figures',
   author = 'Miquel Perello Nieto',
   author_email = 'perello.nieto@gmail.com',
   url = 'https://github.com/perellonieto/DiaryPy',
-  download_url = 'https://github.com/perellonieto/DiaryPy/archive/0.3.tar.gz',
+  download_url = 'https://github.com/perellonieto/DiaryPy/archive/{}.tar.gz'.format(main_ns['__version__']),
   keywords = ['diary', 'notebook', 'logging', 'figures', 'experiments'],
   classifiers = [],
+  long_description=long_description
 )
