ecb19abfcb6e67b58e1d537738b687f705617663
miquel_perello_nieto <perello.nieto@gmail.com>
2020-02-07 13:02:35 +0000

    Improved diary - Save aditional information - Description is is json format - Booleans are stored as strings - Can load existing diaries and look at the history of every notebook - Added util functions to load all diaries with a particular name

--- diarypy/__init__.py
+++ diarypy/__init__.py
@@ -1,1 +1,5 @@
 __version__="0.3.1"
+DESCR_FILENAME='description.txt'
+DIARY_VERSION_DELIMITER='-'
+QUOTECHAR='|'
+DELIMITER=','
--- diarypy/diary.py
+++ diarypy/diary.py
@@ -10,29 +10,57 @@ import os
 import sys
 import errno
 import csv
+import json
 import datetime
 
-try:
-    import PIL.Image as Image
-except ImportError:
-    import Image
+from diarypy import DESCR_FILENAME
+from diarypy import DIARY_VERSION_DELIMITER
+
+
+def preprocess_row(row):
+    if type(row) is dict:
+        row = sum([[key, str(value).replace('\n', '\\n')] for key, value in row.items()], [])
+    for i, element in enumerate(row):
+        if isinstance(element, bool):
+            row[i] = 'True' if element else 'False'
+
+
+def postprocess_row(row):
+    for i, element in enumerate(row):
+        if isinstance(element, str) and (element in ['True', 'False']):
+            row[i] = bool(element)
+
 
 class Notebook(object):
-    def __init__(self, name, diary, verbose=False):
+    def __init__(self, name, diary, verbose=False, header=None, mode='w'):
         self.name = name
         self.filename = "{}.csv".format(name)
         self.diary = diary
         self.entry_number = 0
         self.verbose = verbose
+        self.history = []
+        self.mode = mode
+        if header is not None:
+            with open(os.path.join(self.diary.path, self.filename), 'a') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',', quotechar='|',
+                                    quoting=csv.QUOTE_NONNUMERIC)
+                row = ['id1', 'id2', 'date', 'time'] + header
+                writer.writerow(row)
+
+        if mode == 'r':
+            self.__load_notebook(name)
 
     def add_entry(self, row):
+        if self.mode == 'r':
+            print('Error: Entry not added. This notebook is in read mode only.')
+            return
+
         general_entry_number = self.diary.increase_entry_number()
         self.entry_number += 1
-        if type(row) is dict:
-            row = sum([[key, str(value).replace('\n', '\\n')] for key, value in row.items()], [])
+        preprocess_row(row)
         with open(os.path.join(self.diary.path, self.filename), 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', quotechar='|',
-                    quoting=csv.QUOTE_NONNUMERIC)
+                                quoting=csv.QUOTE_NONNUMERIC)
             now = datetime.datetime.now()
             row = [general_entry_number, self.entry_number,
                     now.date().__str__(),
@@ -41,28 +69,45 @@ class Notebook(object):
         if self.verbose:
             print(row)
 
+    def __load_notebook(self, name):
+        with open(os.path.join(self.path, self.filename), 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|',
+                                quoting=csv.QUOTE_NONNUMERIC)
+            for row in reader:
+                postprocess_row(row)
+                self.history.append(row)
+
+    @property
+    def path(self):
+        return self.diary.path
+
+
 class Diary(object):
 
-    __DESCR_FILENAME='description.txt'
-
     def __init__(self, name, path='diary', overwrite=False, image_format='png',
-                 fig_format='svg', stdout=True, stderr=True, fig_entry=False):
+                 fig_format='svg', stdout=True, stderr=True, fig_entry=False,
+                 mode='w'):
         '''
         Parameters
         ==========
         fig_entry : bool
             If True the name of the figure contains the entry number
         '''
+        if name is None:
+            self.__load_diary(path, mode=mode)
+            return
+
         self.creation_date = datetime.datetime.now()
         self.name = name
-        self.path = os.path.join(path,name)
+        self.root = path
+        self.version = 0
         self.overwrite = overwrite
+        self.mode = mode
 
         self.image_format = image_format
         self.fig_format = fig_format
         self.entry_number = 0
         self.fig_entry = fig_entry
-
         self.all_paths = self._create_all_paths(overwrite)
         self._save_description()
 
@@ -75,6 +120,31 @@ class Diary(object):
             self.redirect_stderr(self.path)
 
         self.notebooks = {}
+
+    def __load_diary(self, path, mode='r'):
+        with open(os.path.join(path, DESCR_FILENAME), 'r') as fp:
+            all_attributes = json.load(fp)
+            for attribute, value in all_attributes.items():
+                self.__dict__[attribute] = value
+        self.notebooks = self.__load_notebooks()
+
+    def __load_notebooks(self):
+        notebooks = {}
+        for filename in os.listdir(self.path):
+            if filename.endswith(".csv"):
+                name = os.path.splitext(filename)[0]
+                notebooks[name] = Notebook(name, diary=self, mode='r')
+        return notebooks
+
+    @property
+    def path(self):
+        return '{}{}{}'.format(os.path.join(self.root, self.name),
+                               DIARY_VERSION_DELIMITER,
+                               self.version)
+
+    @property
+    def all_attributes(self):
+            return dict(vars(self), path=self.path)
 
     def redirect_stdout(self, path, filename='stdout.txt'):
         sys.stdout = open(os.path.join(path, filename), 'w')
@@ -89,18 +159,15 @@ class Diary(object):
     def _create_all_paths(self, overwrite):
         original_path = self.path
         created = False
-        i = 0
         while not created:
             while overwrite == False and os.path.exists(self.path):
-                self.path = "{}_{}".format(original_path,i)
-                i +=1
+                self.version +=1
 
             self.path_images = os.path.join(self.path, 'images')
             self.path_figures = os.path.join(self.path, 'figures')
-            all_paths = [self.path, self.path_images, self.path_figures]
+            all_paths = [self.path,]
             try:
-                for actual_path in all_paths:
-                    os.makedirs(actual_path)
+                os.makedirs(self.path)
                 created = True
             except OSError as exception:
                 if exception.errno != errno.EEXIST:
@@ -108,9 +175,9 @@ class Diary(object):
         return all_paths
 
     def _save_description(self):
-        with open(os.path.join(self.path, self.__DESCR_FILENAME), 'w') as f:
-            print("Writting :\n{}".format(self))
-            f.write(self.__str__())
+        with open(os.path.join(self.path, DESCR_FILENAME), 'w') as fp:
+            print("Saving :\n{}".format(self))
+            json.dump(self.__dict__, fp, indent=1, default=str)
 
     def add_entry(self, notebook_name, row):
         self.notebooks[notebook_name].add_entry(row)
@@ -120,6 +187,13 @@ class Diary(object):
         return self.entry_number
 
     def save_image(self, image, filename='', extension=None):
+        if self.path_images not in self.all_paths:
+            try:
+                os.makedirs(self.path_images)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
+            self.all_paths.append(self.path_images)
         if extension == None:
             extension = self.image_format
         image.save(os.path.join(self.path_images,
@@ -129,6 +203,13 @@ class Diary(object):
     # TODO add support to matplotlib.pyplot.figure or add an additional
     # function
     def save_figure(self, fig, filename=None, extension=None):
+        if self.path_figures not in self.all_paths:
+            try:
+                os.makedirs(self.path_figures)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
+            self.all_paths.append(self.path_figures)
         if extension == None:
             extension = self.fig_format
         fig.tight_layout()
@@ -143,10 +224,8 @@ class Diary(object):
         fig.savefig(os.path.join(self.path_figures, filename))
 
     def __str__(self):
-        return ("Date: {}\nName : {}\nPath : {}\nOverwrite : {}\n"
-                "Image_format : {}\nFigure_format : {}"
-                "").format(self.creation_date, self.name, self.path,
-                        self.overwrite, self.image_format, self.fig_format)
+        return json.dumps(self.all_attributes, indent=1, default=str,
+                          sort_keys=True)
 
     def get_shared_vars(self):
         s_vars = vars(self)
@@ -183,6 +262,11 @@ if __name__ == "__main__":
     diary.add_entry('validation', ['accuracy', 0.5])
     diary.add_entry('validation', ['accuracy', 0.9])
     diary.add_entry('test', ['First test went wrong', 0.345, 'label_1'])
+
+    try:
+        import PIL.Image as Image
+    except ImportError:
+        import Image
 
     image = Image.new(mode="1", size=(16,16), color=0)
     diary.save_image(image, filename='test_results')
