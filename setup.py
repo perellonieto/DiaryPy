21ca658f70a8656f8212729b0ef53b2e74d1f093
Miquel Perello Nieto <perello.nieto@gmail.com>
2020-05-22 10:21:12 +0100

    Reformated readme

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
 from distutils.util import convert_path
+import setuptools
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -9,20 +9,21 @@ ver_path = convert_path('diarypy/version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
-setup(
-  name = 'diarypy',
-  packages = ['diarypy'],
-  install_requires=[
-    'datetime',
-    'Image',
-  ],
-  version=main_ns['__version__'],
-  description = 'Diary to create notebooks and store intermediate results and figures',
-  author = 'Miquel Perello Nieto',
-  author_email = 'perello.nieto@gmail.com',
-  url = 'https://github.com/perellonieto/DiaryPy',
-  download_url = 'https://github.com/perellonieto/DiaryPy/archive/{}.tar.gz'.format(main_ns['__version__']),
-  keywords = ['diary', 'notebook', 'logging', 'figures', 'experiments'],
-  classifiers = [],
-  long_description=long_description
+setuptools.setup(
+    name='diarypy',
+    version=main_ns['__version__'],
+    author='Miquel Perello Nieto',
+    author_email='perello.nieto@gmail.com',
+    description='Diary to create notebooks and store intermediate results and figures',
+    long_description=long_description,
+    url='https://github.com/perellonieto/DiaryPy',
+    packages=setuptools.find_packages(),
+    download_url='https://github.com/perellonieto/DiaryPy/archive/{}.tar.gz'.format(main_ns['__version__']),
+    keywords=['diary', 'notebook', 'logging', 'figures', 'experiments'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
